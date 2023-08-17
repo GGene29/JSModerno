3cb62409bb8a2881d513ffc3f3cc62fc39d6f6d5
GGene29 <sumozacalcurian@gmail.com>
2023-08-17 16:32:57 -0400

    [FIX]-practicas del DOM

---
+++ Curso JS Moderno/13-DOM/js/01-scripts.js
@@ -0,0 +1,17 @@
+// console.log(`Pruebas de JavaScript`);
+
+// let newdiv = document.querySelectorAll('div.logo');
+// console.log(newdiv);
+
+// let container = document.querySelector('div');
+// console.log(container);
+var encabezado = document.querySelector('.contenido-hero h1').textContent;
+console.log(encabezado)
+// console.log(encabezado.innerText);
+// console.log(encabezado.innerHTML);
+// console.log(encabezado.textContent);
+
+
+
+// console.log("hello")
+
