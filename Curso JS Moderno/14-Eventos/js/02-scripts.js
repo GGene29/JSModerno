8e51ed2778e5e5596e9cbf9a2a7882bd7a98ceb6
GGene29 <sumozacalcurian@gmail.com>
2023-09-07 10:31:24 -0400

    [FIX]-Completando los eventos con Mouse

--- Curso JS Moderno/14-Eventos/js/02-scripts.js
+++ Curso JS Moderno/14-Eventos/js/02-scripts.js
@@ -9,16 +9,29 @@ for ( let direcciones of nav ) {
     direcciones.addEventListener('mouseenter', () => {
         console.log('Pasaron sobre mi') //cursor sobre el elemento 'mouseenter'
         //Muy util con estilos sobre los elementos
-        direcciones.style.color='red';    
+        direcciones.style.color='red'; 
+        direcciones.style.textDecorationLine
+        ='underline';
     });
 
     //saliendo del elemento
     direcciones.addEventListener('mouseout', () => {
         console.log('Me fui XD'); //una vez se quita el cursor
         direcciones.style.color='white';
+        direcciones.style.textDecorationLine
+        ='';
         
     });
 
+    //Seleccionando el elemento y lo soltamos
+    direcciones.addEventListener('mouseup', () => {
+        console.log('Apreton');//una vez seleccionen
+        direcciones.style.color='yellow';
+        direcciones.style.textDecorationLine
+        ='underline';
+        
+    });
+    
 } 
 
 
@@ -29,4 +42,29 @@ logotipo.addEventListener('click', () => {
     console.log('¡Me apretaron!'); //click al elemento
 });
 
-//Aqui nuevo
+//seleccion del titulo central
+const titulo = document.querySelector('h1');
+//Evento muy similar al click
+titulo.addEventListener('mousedown', () => {
+    console.log('Entraron');
+    titulo.style.color='blue';
+});
+
+
+//Seleccion del boton buscar
+const botonRed = document.querySelector('.hacer h2');
+console.log(botonRed);
+// Evento doble click
+botonRed.addEventListener('dblclick', ()=>{
+    console.log('Realizando Busqueda');
+    botonRed.style.color='gray';
+    
+});
+
+/*
+mousedown --> similar al click,  seleccion
+click
+dblclick --> Funciona como el doble click
+mouseup --> cuendo se suelta la seleccion del mouse
+mouseenter --> curso sobre el elemento
+*/
