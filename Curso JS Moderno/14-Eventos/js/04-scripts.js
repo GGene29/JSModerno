628b8fc56f9225aed993a4152ae58a41212c841c
GGene29 <sumozacalcurian@gmail.com>
2023-09-07 15:28:12 -0400

    [FIX]-Se agregaron los eventos de formularios

--- Curso JS Moderno/14-Eventos/js/04-scripts.js
+++ Curso JS Moderno/14-Eventos/js/04-scripts.js
@@ -4,6 +4,8 @@
 
 const formulario = document.querySelector('#formulario');
 
+//Una funcion fantasma o anonima
+/*
 formulario.addEventListener('submit', (e) => {
     e.preventDefault(); //Se previene la accion que realiza el evento por defecto
     //Y luego se le pasan lo que queremos que haga
@@ -11,6 +13,17 @@ formulario.addEventListener('submit', (e) => {
 
     //Podemos ver la accion que ejecuta
     console.log(e.target.action);
-
-
 });
+*/
+
+//Con una funcion declarada
+formulario.addEventListener('submit', validandoImput);
+
+function validandoImput(event){
+    event.preventDefault(); //Se previene la accion que realiza el evento por defecto
+    //Y luego se le pasan lo que queremos que haga
+    console.log('Buscando la información');
+    // console.log(event.target);
+    //Podemos ver la accion que ejecuta
+    console.log(event.target.action);
+}
