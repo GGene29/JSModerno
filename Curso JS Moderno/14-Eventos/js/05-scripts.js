e9bb87905225303e8548c3e26edf37a84a4ed0fd
GGene29 <sumozacalcurian@gmail.com>
2023-09-27 11:23:56 -0400

    nueva rama

---
+++ Curso JS Moderno/14-Eventos/js/05-scripts.js
@@ -0,0 +1,23 @@
+//Eventos con el scrol 
+
+//Los cuales se ejecutan acorde a la cantidad de scroll o la posición del mismo
+
+//por medio de window, tenemos acceso a todo el documento
+window.addEventListener('scroll', () => {
+    // scrollX horizontal
+    // scrollY vertical
+    console.log('scrolling');
+
+    const scrollEnPixeles = window.scrollY; //Busqueda vertical
+    console.log(scrollEnPixeles)
+
+    const premiuntarget = document.querySelector('.premium')
+
+    const ubicacionpremiun = premiuntarget.getBoundingClientRect();
+
+    // if (ubicacionpremiun >= 500){
+    //     console.log('Dentro del rango visible')
+    // }else{
+    //     console.log('Fuera de la visibilidad')
+    // }
+})
