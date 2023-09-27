a853cd5931fc4a11ff5eecfd99e54aef6d6733ad
GGene29 <sumozacalcurian@gmail.com>
2023-09-27 12:13:55 -0400

    [FIX]-Agregada animación con el evento scroll

--- Curso JS Moderno/14-Eventos/js/05-scripts.js
+++ Curso JS Moderno/14-Eventos/js/05-scripts.js
@@ -1,4 +1,4 @@
-//Eventos con el scrol 
+//Eventos con el scroll
 
 //Los cuales se ejecutan acorde a la cantidad de scroll o la posición del mismo
 
@@ -6,19 +6,25 @@
 window.addEventListener('scroll', () => {
     // scrollX horizontal
     // scrollY vertical
-    console.log('scrolling');
+    // console.log('scrolling');
 
     const scrollEnPixeles = window.scrollY; //Busqueda vertical
-    console.log(scrollEnPixeles)
+    // console.log(scrollEnPixeles);
 
-    const premiuntarget = document.querySelector('.premium')
+    const premiuntarget = document.querySelector('.premium');
+    // console.log(premiuntarget);
+    const premium = premiuntarget.getBoundingClientRect();
+    console.log(premium); //Se podría trabajar con el top o el botton
 
-    const ubicacionpremiun = premiuntarget.getBoundingClientRect();
 
-    // if (ubicacionpremiun >= 500){
-    //     console.log('Dentro del rango visible')
-    // }else{
-    //     console.log('Fuera de la visibilidad')
-    // }
+    if (premium.top < 281){
+        console.log('Dentro del rango visible')
+        premiuntarget.childNodes[1].style='color:gray;'
+        
+        console.log(premiuntarget.childNodes[1])
+    }else{
+        console.log('Fuera de la visibilidad')
+        premiuntarget.childNodes[1].style='color:red;'
+    }
     //new residencia, fusionado con cambios
 })
