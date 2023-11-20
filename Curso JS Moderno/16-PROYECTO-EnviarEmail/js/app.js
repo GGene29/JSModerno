73703b19e41aa6ccee74d234778f75ece6945e89
GGene29 <sumozacalcurian@gmail.com>
2023-11-20 16:21:57 -0400

    [FIX]-Validando cada campo

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
         
     
 /*  ======================================
@@ -32,18 +33,38 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e){
+        
+
         // vacioInput = e.target.value;
         //trim es un metodo de string, para eliminar espacios en blanco
         if (e.target.value.trim() === ''){
             console.log('campo obligatorio');
             console.log(e.target.id);
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
-        } else{
-            console.log(e.target.value);
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return;
         }
+
+        limpiarAlerta(e.target.parentElement)
+
+        console.log('despues del if')
+        
     };
     
-    function mostrarAlerta(mensaje){
+    function mostrarAlerta(mensaje, referencia){
+        //parte 2 pero EFECTIVA
+        // limpiarAlerta(e.target.parentElement)
+        limpiarAlerta(referencia)
+        
+        //parte 1
+        //Comprobar existencia de alerta (para tener solo una)
+        // const alerta = referencia.querySelector('.bg-red-600')
+        // console.log(alerta)
+        // if (alerta){
+        //     alerta.remove()
+        // }
+
+
+
         //creacion de Html
         const error = document.createElement('P');
         //Asignacion del mensaje
@@ -51,8 +72,17 @@ document.addEventListener('DOMContentLoaded', function(){
         //Agregar clases
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
         //Añade a html la notificacion del campo
-        formulario.appendChild(error);
+        referencia.appendChild(error);
     };
+
+    function limpiarAlerta(referencia) {
+        console.log('limpiando')
+        const alerta = referencia.querySelector('.bg-red-600');
+        console.log(alerta)
+        if (alerta){
+            alerta.remove();
+        }
+    }
 
     //blur, input, cut, copy, keydown, keyup, paste  
     //con todos los cambios
